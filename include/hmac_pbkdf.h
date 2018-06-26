7cfa4b6502b0e744bf126f498f6aaea2c8592529
korLethean <hdgwon@naver.com>
2018-06-26 14:31:26 +0900

    HMAC PBKDF digest commit

--- include/hmac_pbkdf.h
+++ include/hmac_pbkdf.h
@@ -14,13 +14,12 @@
 #define FB_MODE 2
 #define DP_MODE 3
 
-lsh_err hmac_kdf_ctr_digest();
+lsh_err hmac_kdf_ctr_digest(lsh_type algtype, int loop_count, lsh_u8 output);
 
-lsh_err hmac_kdf_fb_digest();
+lsh_err hmac_kdf_fb_digest(lsh_type algtype, int loop_count, lsh_u8 output);
 
-lsh_err hmac_kdf_dp_digest();
+lsh_err hmac_kdf_dp_digest(lsh_type algtype, int loop_count, lsh_u8 output);
 
-lsh_err hmac_kdf_digest(int mode);
-
+lsh_err hmac_kdf_digest(int mode, lsh_type algtype, lsh_u8 *Ki, lsh_u8 *label, lsh_u8 *context, lsh_uint r, lsh_uint len, lsh_uint hash_len);
 
 #endif /* INCLUDE_HMAC_PBKDF_H_ */
--- src/hmac_pbkdf.c
+++ src/hmac_pbkdf.c
@@ -7,37 +7,56 @@
 
 #include "../include/hmac_pbkdf.h"
 
-lsh_err hmac_pbkdf_ctr_digest()
+lsh_err hmac_pbkdf_ctr_digest(lsh_type algtype, int loop_count, lsh_u8 output)
 {
 	lsh_err result;
 
 	return result;
 }
 
-lsh_err hmac_kdf_fb_digest()
+lsh_err hmac_kdf_fb_digest(lsh_type algtype, int loop_count, lsh_u8 output)
 {
 	lsh_err result;
 
 	return result;
 }
 
-lsh_err hmac_kdf_dp_digest()
+lsh_err hmac_kdf_dp_digest(lsh_type algtype, int loop_count, lsh_u8 output)
 {
 	lsh_err result;
 
 	return result;
 }
 
-lsh_err hmac_pbkdf_digest(int mode)
+lsh_err hmac_kdf_digest(int mode, lsh_type algtype, lsh_u8 *Ki, lsh_u8 *label, lsh_u8 *context, lsh_uint r, lsh_uint len, lsh_uint hash_len)
 {
 	lsh_err result;
+	lsh_u8 *digest;
+	double n;
+	int bit_unit;
+
+	n = ceil((double)len / (double) hash_len);
+
+	//malloc
 
 	if(mode == CTR_MODE)
-		hmac_pbkdf_ctr_digest();
+	{
+		result = hmac_pbkdf_ctr_digest(algtype, n);
+		if(result != LSH_SUCCESS)
+			return result;
+	}
 	else if(mode == FB_MODE)
-		hmac_pbkdf_fb_digest();
+	{
+		result = hmac_pbkdf_fb_digest(algtype, n);
+		if(result != LSH_SUCCESS)
+			return result;
+	}
 	else if(mode == DP_MODE)
-		hmac_pbkdf_dp_digest();
+	{
+		result = hmac_pbkdf_dp_digest(algtype, n);
+		if(result != LSH_SUCCESS)
+			return result;
+	}
 	else
 		printf("unknown mode \n");
 

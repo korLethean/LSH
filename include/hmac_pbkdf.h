cf3ed9d9136d57a914ff5736a1407286f2213613
korLethean <hdgwon@naver.com>
2018-06-26 14:37:33 +0900

    get output file path from parameter

--- include/hmac_pbkdf.h
+++ include/hmac_pbkdf.h
@@ -14,11 +14,12 @@
 #define FB_MODE 2
 #define DP_MODE 3
 
-lsh_err hmac_kdf_ctr_digest(lsh_type algtype, int loop_count, lsh_u8 output);
+lsh_err hmac_kdf_ctr_digest(lsh_type algtype, int loop_count, lsh_u8 output, FILE *fp);
 
-lsh_err hmac_kdf_fb_digest(lsh_type algtype, int loop_count, lsh_u8 output);
+lsh_err hmac_kdf_fb_digest(lsh_type algtype, int loop_count, lsh_u8 output, FILE *fp);
 
-lsh_err hmac_kdf_dp_digest(lsh_type algtype, int loop_count, lsh_u8 output);
+lsh_err hmac_kdf_dp_digest(lsh_type algtype, int loop_count, lsh_u8 output, FILE *fp);
 
-lsh_err hmac_kdf_digest(int mode, lsh_type algtype, lsh_u8 *Ki, int Ki_len, lsh_u8 *label, int label_len, lsh_u8 *context, int context_len, lsh_uint r, lsh_uint len, lsh_uint hash_len);
+lsh_err hmac_kdf_digest(int mode, lsh_type algtype, lsh_u8 *Ki, int Ki_len, lsh_u8 *label, int label_len, lsh_u8 *context, int context_len, lsh_uint r, lsh_uint len, lsh_uint hash_len, FILE *fp);
+
 #endif /* INCLUDE_HMAC_PBKDF_H_ */
--- src/hmac_pbkdf.c
+++ src/hmac_pbkdf.c
@@ -7,7 +7,7 @@
 
 #include "../include/hmac_pbkdf.h"
 
-lsh_err hmac_pbkdf_ctr_digest(lsh_type algtype, int loop_count, lsh_u8 output)
+lsh_err hmac_pbkdf_ctr_digest(lsh_type algtype, int loop_count, lsh_u8 output, FILE *fp)
 {
 	lsh_err result;
 	lsh_u8 *input;
@@ -17,21 +17,21 @@ lsh_err hmac_pbkdf_ctr_digest(lsh_type algtype, int loop_count, lsh_u8 output)
 	return result;
 }
 
-lsh_err hmac_kdf_fb_digest(lsh_type algtype, int loop_count, lsh_u8 output)
+lsh_err hmac_kdf_fb_digest(lsh_type algtype, int loop_count, lsh_u8 output, FILE *fp)
 {
 	lsh_err result;
 
 	return result;
 }
 
-lsh_err hmac_kdf_dp_digest(lsh_type algtype, int loop_count, lsh_u8 output)
+lsh_err hmac_kdf_dp_digest(lsh_type algtype, int loop_count, lsh_u8 output, FILE *fp)
 {
 	lsh_err result;
 
 	return result;
 }
 
-lsh_err hmac_kdf_digest(int mode, lsh_type algtype, lsh_u8 *Ki, int Ki_len, lsh_u8 *label, int label_len, lsh_u8 *context, int context_len, lsh_uint r, lsh_uint len, lsh_uint hash_len)
+lsh_err hmac_kdf_digest(int mode, lsh_type algtype, lsh_u8 *Ki, int Ki_len, lsh_u8 *label, int label_len, lsh_u8 *context, int context_len, lsh_uint r, lsh_uint len, lsh_uint hash_len, FILE *fp)
 {
 	lsh_err result;
 	lsh_u8 *digest;
@@ -44,19 +44,19 @@ lsh_err hmac_kdf_digest(int mode, lsh_type algtype, lsh_u8 *Ki, int Ki_len, lsh_
 
 	if(mode == CTR_MODE)
 	{
-		result = hmac_pbkdf_ctr_digest(algtype, n);
+		result = hmac_pbkdf_ctr_digest(algtype, n, digest, fp);
 		if(result != LSH_SUCCESS)
 			return result;
 	}
 	else if(mode == FB_MODE)
 	{
-		result = hmac_pbkdf_fb_digest(algtype, n);
+		result = hmac_pbkdf_fb_digest(algtype, n, digest, fp);
 		if(result != LSH_SUCCESS)
 			return result;
 	}
 	else if(mode == DP_MODE)
 	{
-		result = hmac_pbkdf_dp_digest(algtype, n);
+		result = hmac_pbkdf_dp_digest(algtype, n, digest, fp);
 		if(result != LSH_SUCCESS)
 			return result;
 	}
